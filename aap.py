2d637becdc68635dcd92a12e77ca26feaafc65aa
navneetshukla17 <tusharshukla707@gmail.com>
2024-08-28 14:51:21 +0530

    some samll changes

--- aap.py
+++ aap.py
@@ -9,9 +9,10 @@ import dash_core_components as dcc
 from dash.dependencies import Input, Output
 
 
+# external CSS stylesheets
 external_stylesheets = [
     {
-        'href': 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
+        'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
         'rel': 'stylesheet',
         'integrity': 'sha384-KyZXEAg3QhqLMpG8r+Knujsl5/WTdOihzJjibJxy1zqD4wDibL0c5o49yIf6Yyoi',
         'crossorigin': 'anonymous'
@@ -19,3 +20,52 @@ external_stylesheets = [
 ]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    html.H1('Corona Virus Pandemic', style={'color':'#fff', 'text-align':'center'}),
+    html.Div([
+        html.Div([
+            html.Div([
+                html.Div([
+                    html.H3('Total Cases', style={'color':'#fff'}, className='text-light'),
+                    html.H4('600', style={'color':'#fff'}, className='text-light')
+                ], className='card-body')
+            ], className='card bg-danger') 
+        ], className='col-md-3'),
+        html.Div([
+            html.Div([
+                html.Div([
+                    html.H3('Total Cases', style={'color':'#fff'}, className='text-light'),
+                    html.H4('600', style={'color':'#fff'}, className='text-light')
+                ], className='card-body')
+            ], className='card bg-danger')], className='col-md-3'),
+        html.Div([
+            html.Div([
+                html.Div([
+                    html.H3('Total Cases', style={'color':'#fff'}, className='text-light'),
+                    html.H4('600', style={'color':'#fff'}, className='text-light')
+                ], className='card-body')
+            ], className='card bg-danger')], className='col-md-3'),
+        html.Div([
+            html.Div([
+                html.Div([
+                    html.H3('Total Cases', style={'color':'#fff'}, className='text-light'),
+                    html.H4('600', style={'color':'#fff'}, className='text-light')
+                ], className='card-body')
+            ], className='card bg-danger')], className='col-md-3')
+    ], className='row'),
+    html.Div([
+        html.Div([], className='col-md-6'),
+        html.Div([], className='col-md-6')
+    ], className='row'),
+    html.Div([
+        html.Div([], className='col-md-12')
+    ], className='row'),
+], className='container')
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+    
+    
